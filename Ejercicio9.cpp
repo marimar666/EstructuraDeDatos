19bb21ad8d69806736716e473bfe163e29ccb62e
marimar666 <131387205+marimar666@users.noreply.github.com>
2023-08-18 12:53:20 -0500

    Create Ejercicio9.cpp

---
+++ Ejercicio9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Bienvenido a C++;" << endl;
+    cout << "Pronto comenzaremos a programar en C." << endl;
+
+    return 0;
+}
